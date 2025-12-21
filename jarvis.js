d5747854d238bd0724e3778e3734134c2f2daa50
Projetoholyrics <nadatemeres@gmail.com>
2025-12-21 01:01:57 -0300

    Add files via upload

--- jarvis.js
+++ jarvis.js
@@ -313,3 +313,9 @@ function iniciarProtocoloJarvis() {
     setState("idle");
   }, 11000);
 }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(() => console.log("Service Worker registrado com sucesso"))
+    .catch((err) => console.log("Erro no Service Worker", err));
+}
